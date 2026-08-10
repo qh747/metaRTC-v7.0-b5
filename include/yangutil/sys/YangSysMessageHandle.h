fafbf10ddcc722edaad70cc2bdd1ab704454a863
quhan <2217362869@qq.com>
2026-08-11 02:26:19 +0800

    fix: 修复 YangSysMessageHandle 多线程竞态

--- include/yangutil/sys/YangSysMessageHandle.h
+++ include/yangutil/sys/YangSysMessageHandle.h
@@ -1,43 +1,94 @@
-﻿
+
 //
 // Copyright (c) 2019-2025 yanggaofeng
 //
 #ifndef INCLUDE_YANGUTIL_SYS_YANGSYSMESSAGEHANDLE_H_
 #define INCLUDE_YANGUTIL_SYS_YANGSYSMESSAGEHANDLE_H_
-#include <yangutil/sys/YangSysMessageI.h>
-#include <yangutil/sys/YangThread2.h>
+
+#include <atomic>
 #include <vector>
+#include <yangutil/sys/YangThread2.h>
+#include <yangutil/sys/YangSysMessageI.h>
 
-
-using namespace std;
-
-class YangSysMessageHandle :public YangThread{
+class YangSysMessageHandle : public YangThread {
 public:
+	/**
+	 * 构造函数
+	 */
 	YangSysMessageHandle();
-	virtual ~YangSysMessageHandle();
-	virtual void handleMessage(YangSysMessage* mss)=0;
-	virtual void initAll()=0;
-	virtual void deleteAll()=0;
-	void putMessage(YangSysMessageI *handle,int32_t pst, int32_t puid, int32_t handleState,void* user=NULL);
 
-	static YangSysMessageHandle* m_instance;
-	yangbool m_isStart;
-	yangbool m_loop;
-	void stop();
+	/**
+	 * 析构函数
+	 */
+	virtual ~YangSysMessageHandle();
+
+public:
+    /**
+	 * 停止线程
+	 */
+    virtual void stop();
+
 protected:
-	void run();
+    /**
+	 * 运行线程任务
+	 */
+	virtual void run();
+
+public:
+    /**
+	 * 处理系统消息
+	 * @param mss 系统消息
+	 */
+	virtual void handleMessage(YangSysMessage* mss) = 0;
+	
+	/**
+	 * 初始化所有系统消息
+	 */
+	virtual void initAll() = 0;
+
+	/**
+	 * 删除所有系统消息
+	 */
+	virtual void deleteAll() = 0;
+
+public:
+	/**
+	 * 放入系统消息
+	 * @param handle 系统消息处理接口
+	 * @param pst 系统消息类型
+	 * @param uid 系统消息用户ID
+	 * @param handleState 系统消息处理状态
+	 * @param user 系统消息用户数据
+	 */
+	void putMessage(
+		YangSysMessageI* handle,
+		int32_t pst, 
+		int32_t uid, 
+		int32_t handleState,
+		void* user = NULL);
+
+protected:
+    /**
+	 * 启动系统消息处理循环
+	 */
 	void startLoop();
+	
+	/**
+	 * 停止系统消息处理循环
+	 */
 	void stopLoop();
 
+public:
+	yangbool m_isStart;
+	std::atomic<int32_t> m_loop;
+
 private:
-	vector<YangSysMessage*> m_sysMessages;
+	std::vector<YangSysMessage*> m_sysMessages;
 	yang_thread_mutex_t m_mutex;
 	yang_thread_mutex_t m_lock;
 	yang_thread_cond_t m_cond_mess;
 
-	yangbool m_waitState;
 	YangSysMessageHandleI* m_receive;
-
 };
 
-#endif /* INCLUDE_YANGUTIL_SYS_YANGSYSMESSAGEHANDLE_H_ */
+#endif // INCLUDE_YANGUTIL_SYS_YANGSYSMESSAGEHANDLE_H_
--- libmetartc7/src/yangutil/YangSysMessageHandle.cpp
+++ libmetartc7/src/yangutil/YangSysMessageHandle.cpp
@@ -1,120 +1,124 @@
-﻿//
+//
 // Copyright (c) 2019-2025 yanggaofeng
 //
 
-
-#include <yangutil/sys/YangSysMessageHandle.h>
-
+#include <atomic>
 #include <yangutil/sys/YangLog.h>
 #include <yangutil/sys/YangThread.h>
 #include <yangutil/yangavinfotype.h>
+#include <yangutil/sys/YangSysMessageHandle.h>
 
-void yang_post_message(int32_t st, int32_t uid, YangSysMessageI *mhandle,void* user) {
-	if (YangSysMessageHandle::m_instance)
-		YangSysMessageHandle::m_instance->putMessage(mhandle, st, uid, 0,user);
+static std::atomic<YangSysMessageHandle*> g_instance{nullptr};
+
+void yang_post_message(int32_t st, int32_t uid, YangSysMessageI* handle, void* user) {
+	YangSysMessageHandle* inst = g_instance.load();
+	if (inst) {
+		inst->putMessage(handle, st, uid, 0, user);
+	}
 }
-void yang_post_state_message(int32_t st, int32_t uid, int32_t handleState,YangSysMessageI *mhandle) {
-	if (YangSysMessageHandle::m_instance)
-		YangSysMessageHandle::m_instance->putMessage(mhandle, st, uid,
-				handleState);
+
+void yang_post_state_message(int32_t st, int32_t uid, int32_t handleState, YangSysMessageI* handle) {
+	YangSysMessageHandle* inst = g_instance.load();
+	if (inst) {
+		inst->putMessage(handle, st, uid, handleState);
+	}
 }
-void yang_post_userMessage(int32_t st, int32_t uid,YangSysMessageI *mhandle,void* user){
-    if (YangSysMessageHandle::m_instance)
-        YangSysMessageHandle::m_instance->putMessage(mhandle, st, uid, 0);
-}
+
 YangSysMessageHandle::YangSysMessageHandle() {
+	YangSysMessageHandle* expected = nullptr;
+	g_instance.compare_exchange_strong(expected, this);
+
 	m_loop = yangfalse;
 	m_isStart = yangfalse;
+
 	yang_thread_mutex_init(&m_mutex, NULL);
 	yang_thread_mutex_init(&m_lock, NULL);
 	yang_thread_cond_init(&m_cond_mess, NULL);
-	m_waitState = yangfalse;
-	m_receive=NULL;
-	if (m_instance == NULL)
-		m_instance = this;
+
+	m_receive = NULL;
 }
 
 YangSysMessageHandle::~YangSysMessageHandle() {
-
 	if (m_isStart) {
-		stop();
+		this->stop();
+
 		while (m_isStart) {
 			yang_usleep(1000);
 		}
 	}
-	m_instance = NULL;
+
+	YangSysMessageHandle* expected = this;
+	g_instance.compare_exchange_strong(expected, nullptr);
+
 	yang_thread_mutex_destroy(&m_mutex);
 	yang_thread_mutex_destroy(&m_lock);
 	yang_thread_cond_destroy(&m_cond_mess);
-
 }
-YangSysMessageHandle *YangSysMessageHandle::m_instance = NULL;
+
 void YangSysMessageHandle::run() {
 	m_isStart = yangtrue;
-    startLoop();
+    this->startLoop();
 	m_isStart = yangfalse;
 }
+
 void YangSysMessageHandle::stop() {
-	stopLoop();
-
+	this->stopLoop();
 }
-void YangSysMessageHandle::stopLoop() {
-	m_loop = yangfalse;
-	//yang_thread_mutex_unlock(&m_lock);
-	if(m_waitState){
-		yang_thread_mutex_lock(&m_lock);
-		yang_thread_cond_signal(&m_cond_mess);
-		yang_thread_mutex_unlock(&m_lock);
 
+void YangSysMessageHandle::putMessage(
+	YangSysMessageI* handle, int32_t pst, int32_t uid, int32_t handleState, void* user) {
+	if (!m_loop.load()) {
+		return;
 	}
-}
 
-void YangSysMessageHandle::putMessage(YangSysMessageI *handle, int32_t pst,
-        int32_t puid, int32_t handleState,void* user) {
-	if(!m_loop) return;
+	YangSysMessage* mes = new YangSysMessage();
 
-	YangSysMessage* mes=new YangSysMessage();
-
-	mes->uid = puid;
+	mes->uid = uid;
 	mes->messageId = pst;
 	mes->handleState = handleState;
 	mes->handle = handle;
     mes->user=user;
+
     yang_thread_mutex_lock(&m_mutex);
 	m_sysMessages.push_back(mes);
 	yang_thread_mutex_unlock(&m_mutex);
-	mes=NULL;
 
-	if(m_waitState){
-		yang_thread_mutex_lock(&m_lock);
-		yang_thread_cond_signal(&m_cond_mess);
-		yang_thread_mutex_unlock(&m_lock);
-	}
+	yang_thread_mutex_lock(&m_lock);
+	yang_thread_cond_signal(&m_cond_mess);
+	yang_thread_mutex_unlock(&m_lock);
 }
 
 void YangSysMessageHandle::startLoop() {
-   m_loop = 1;
+    m_loop = yangtrue;
 
-   yang_thread_mutex_unlock(&m_lock);
     yang_thread_mutex_lock(&m_lock);
-
-    while (m_loop) {
-
-        m_waitState=yangtrue;
+    while (m_loop.load()) {
         yang_thread_cond_wait(&m_cond_mess, &m_lock);
-        m_waitState=yangfalse;
 
-        while (m_sysMessages.size()>0) {
-            handleMessage(m_sysMessages.front());
-            m_sysMessages.front()->handle = NULL;
-            delete m_sysMessages.front();
-            m_sysMessages.front() = NULL;
+        while (true) {
             yang_thread_mutex_lock(&m_mutex);
+            if (m_sysMessages.empty()) {
+                yang_thread_mutex_unlock(&m_mutex);
+                break;
+            }
+
+            YangSysMessage* mes = m_sysMessages.front();
             m_sysMessages.erase(m_sysMessages.begin());
             yang_thread_mutex_unlock(&m_mutex);
+        
+            this->handleMessage(mes);
+            mes->handle = NULL;
+            delete mes;
         }
-
     }
     yang_thread_mutex_unlock(&m_lock);
 }
 
+void YangSysMessageHandle::stopLoop() {
+	yang_thread_mutex_lock(&m_lock);
+	
+	m_loop = yangfalse;
+	yang_thread_cond_signal(&m_cond_mess);
+	
+	yang_thread_mutex_unlock(&m_lock);
+}
